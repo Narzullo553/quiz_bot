daebb690c55738cea1ba2d8a8c30d9f3cdb67311
Murodullayev <muranarz@gmail.com>
2025-01-24 14:41:18 +0500

    a

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from loader import dp, db
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
+from dotenv import load_dotenv
 from data.config import ADMINS
 
 async def on_startup(dispatcher):
