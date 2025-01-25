40f288c9edf5cdc30ac5d0442aba79c10181f9e7
Murodullayev <muranarz@gmail.com>
2025-01-25 14:52:28 +0500

    hechnima

--- data/config.py
+++ data/config.py
@@ -7,4 +7,12 @@ env.read_env()
 # .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
+IP = env.str("ip")
+import os
+
+print("Environment Variables:")
+print("BOT_TOKEN:", os.getenv("BOT_TOKEN"))
+print("ADMINS:", os.getenv("ADMINS"))
+print("IP:", os.getenv("ip"))
+
+  # Xosting ip manzili
